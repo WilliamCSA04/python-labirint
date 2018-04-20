f5a865c2737ee8d03299db96e1364d7a005c2981
William Caetano <williamcaetano14@gmail.com>
2018-04-19 22:04:22 -0300

    Added validate_size method

--- labirint.py
+++ labirint.py
@@ -1,10 +1,15 @@
 class Labirint:
 
     def __init__(self, size):
-        self.size = size
+        self.__validate_size(size)
         self.matrix = [["-" if x is 0 or y is 0 or x is size-1 or y is size-1 else "*" for x in range(size)] for y in range(size)]
 
 
-    def printLabirint(self):
+    def print_labirint(self):
         for x in self.matrix:
             print(x)
+
+    def __validate_size(self, size):
+        if(size < 10):
+            size = 10
+        self.size = size
