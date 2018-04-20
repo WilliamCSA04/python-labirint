0279176051918cf44cd5b10dc82cdadb8f078237
William Caetano <williamcaetano14@gmail.com>
2018-04-19 21:56:22 -0300

    Created printLabirint method

--- labirint.py
+++ labirint.py
@@ -3,3 +3,8 @@ class Labirint:
     def __init__(self, size):
         self.size = size
         self.matrix = [["-" if x is 0 or y is 0 or x is size-1 or y is size-1 else "*" for x in range(size)] for y in range(size)]
+
+
+    def printLabirint(self):
+        for x in self.matrix:
+            print(x)
