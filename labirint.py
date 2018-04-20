7ea5ece214e358a5c6b5a5f50af0648a97fcbfaf
William Caetano <williamcaetano14@gmail.com>
2018-04-19 21:47:32 -0300

    Added size as a parameter to labirint

--- labirint.py
+++ labirint.py
@@ -1,6 +1,5 @@
 class Labirint:
 
-    def __init__(self):
-        size = 20
+    def __init__(self, size):
         self.size = size
         self.matrix = [["-" if x is 0 or y is 0 or x is size or y is size else "*" for x in range(size)] for y in range(size)]
