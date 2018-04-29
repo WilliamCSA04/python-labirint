3a41f7e4a706f341a8b96f8d4eb09db7db485fd1
William <williamcaetano14@gmail.com>
2018-04-29 18:29:07 -0300

    Fixed matrix generator size

--- labirint.py
+++ labirint.py
@@ -2,7 +2,7 @@ class Labirint(object):
 
     def __init__(self, size):
         self.__validate_size(size)
-        self.matrix = [["-" if x is 0 or y is 0 or x is size-1 or y is size-1 else " " for x in range(size)] for y in range(size)]
+        self.matrix = [["-" if x is 0 or y is 0 or x is self.size-1 or y is self.size-1 else " " for x in range(self.size)] for y in range(self.size)]
 
     def print_labirint(self):
         for x in self.matrix:
@@ -11,4 +11,5 @@ class Labirint(object):
     def __validate_size(self, size):
         if(size < 10):
             size = 10
+        print(size)
         self.size = size
