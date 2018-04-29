3295e93ba98604450f48fccb049f0c0ba74bebf8
William <williamcaetano14@gmail.com>
2018-04-29 20:54:09 -0300

    Fixed bug: Beginning and Ending positions were defined at even positions

--- labirint.py
+++ labirint.py
@@ -1,5 +1,5 @@
 import random
-from helper import is_even
+from helper import *
 class Labirint:
 
     __MIN_SIZE = 11
@@ -41,7 +41,7 @@ class Labirint:
             for index_column, y in enumerate(x):
                 should_generate = random.random() > 0.9
                 not_a_corner = (index_row != 0 or index_column != 0) and (index_row != 0 or index_column != border) and (index_row != border or index_column != 0) and (index_row != border or index_column != border) 
-                is_a_valid_position = (index_row is 0 or index_column is 0 or index_row is border or index_column is border) and not_a_corner                
+                is_a_valid_position = (index_row is 0 or index_column is 0 or index_row is border or index_column is border) and not_a_corner and (is_odd(index_row) or is_odd(index_column))               
                 can_generate_a_initial_ending_position = is_a_valid_position and should_generate
                 if can_generate_a_initial_ending_position:
                     if beginning_done:
