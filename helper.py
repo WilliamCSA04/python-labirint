dd8a9f6ab6133f186fdbc77143feb17c23a3a16d
William Caetano <williamcaetano14@gmail.com>
2018-05-10 20:12:21 -0300

    Changed method to generate next coordinates

--- helper.py
+++ helper.py
@@ -6,7 +6,9 @@ def is_even(number):
 def is_odd(number):
     return number % 2 == 1
 
-def next_step_randomly(coordinates):
-     next_column = random.randint(-1, 1)
-     next_row = random.randint(0, 1)
-     return [next_row, next_column]
+def next_step_randomly_without_diagonals(coordinates):
+    next_column = random.randint(-1, 1)
+    next_row = random.randint(0, 1)
+    if next_column is 1 or next_column is -1:
+        next_row = 0
+    return [next_row, next_column]
