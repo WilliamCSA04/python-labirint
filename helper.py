0090b4db8350924469b59d7d57b246fcf8701fac
William Caetano <williamcaetano14@gmail.com>
2018-05-10 20:04:18 -0300

    Added helper method to generate a random coordinate

--- helper.py
+++ helper.py
@@ -1,5 +1,12 @@
+import random
+
 def is_even(number):
     return number % 2 == 0
 
 def is_odd(number):
     return number % 2 == 1
+
+def next_step_randomly(coordinates):
+     next_column = random.randint(-1, 1)
+     next_row = random.randint(0, 1)
+     return [next_row, next_column]
