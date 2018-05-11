0216a874eb00e56e7a3f0dc4122b08808a3a1a1a
William Caetano <williamcaetano14@gmail.com>
2018-05-11 19:25:56 -0300

    Added method to return corner_direction

--- helper.py
+++ helper.py
@@ -12,3 +12,14 @@ def next_step_randomly_without_diagonals(coordinates):
     if next_column is 1 or next_column is -1:
         next_row = 0
     return [next_row, next_column]
+
+def corner_direction(cellNorth, cellSouth, cellEast, cellWest):
+    if(cellNorth == cellEast):
+        return "NE"
+    if(cellNorth == cellWest):
+        return "NW"
+    if(cellSouth == cellWest):
+        return "SW"
+    if(cellSouth == cellEast):
+        return "SE"
+    return None
