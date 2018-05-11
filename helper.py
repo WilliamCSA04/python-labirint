602c6f423c8b5cf5484939c6dad4cceedcc80424
William Caetano <williamcaetano14@gmail.com>
2018-05-11 19:31:00 -0300

    Added removed non generical helper methods from helper

--- helper.py
+++ helper.py
@@ -5,21 +5,3 @@ def is_even(number):
 
 def is_odd(number):
     return number % 2 == 1
-
-def next_step_randomly_without_diagonals(coordinates):
-    next_column = random.randint(-1, 1)
-    next_row = random.randint(0, 1)
-    if next_column is 1 or next_column is -1:
-        next_row = 0
-    return [next_row, next_column]
-
-def corner_direction(cellNorth, cellSouth, cellEast, cellWest):
-    if(cellNorth == cellEast):
-        return "NE"
-    if(cellNorth == cellWest):
-        return "NW"
-    if(cellSouth == cellWest):
-        return "SW"
-    if(cellSouth == cellEast):
-        return "SE"
-    return None
