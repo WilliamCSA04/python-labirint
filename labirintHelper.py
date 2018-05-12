57e644833ecbfe79ea59393d20cbfbc725f1f4ed
William Caetano <williamcaetano14@gmail.com>
2018-05-11 23:31:18 -0300

    Moved new_valid_coordinates from labirintHelper to labirintService

--- labirintHelper.py
+++ labirintHelper.py
@@ -8,26 +8,3 @@ def next_step_randomly_without_diagonals(coordinates):
             next_row = 0
         return [next_row, next_column]
     return random.choice(coordinates)
-
-def new_valid_coordinates(cell_north, cell_south, cell_east, cell_west):
-    validator_character = "@"
-    is_a_vertical_path = validator_character == cell_north == cell_south
-    is_a_horizontal_path = validator_character == cell_west == cell_east
-    is_a_path =  is_a_vertical_path or is_a_horizontal_path
-    if is_a_path:
-        return None
-    possibilities = []
-    empty_cell = " "
-    if(cell_north == empty_cell):
-        north = [-1, -1]
-        possibilities.append(north)
-    if(cell_west == empty_cell):
-        west = [0, -1]
-        possibilities.append(west)
-    if(cell_west == empty_cell):
-        east = [0, 1]
-        possibilities.append(east)
-    if(cell_south == cell_east):
-        south = [1, 1]
-        possibilities.append(south)
-    return possibilities
--- labirintService.py
+++ labirintService.py
@@ -23,3 +23,27 @@ class LabirintService:
         if coordinate[1] is 0:
             return None
         return self.matrix[coordinate[0], self.matrix[1]-1]
+
+    
+    def new_valid_coordinates(self, cell_north, cell_south, cell_east, cell_west):
+        validator_character = "@"
+        is_a_vertical_path = validator_character == cell_north == cell_south
+        is_a_horizontal_path = validator_character == cell_west == cell_east
+        is_a_path =  is_a_vertical_path or is_a_horizontal_path
+        if is_a_path:
+            return None
+        possibilities = []
+        empty_cell = " "
+        if(cell_north == empty_cell):
+            north = [-1, -1]
+            possibilities.append(north)
+        if(cell_west == empty_cell):
+            west = [0, -1]
+            possibilities.append(west)
+        if(cell_west == empty_cell):
+            east = [0, 1]
+            possibilities.append(east)
+        if(cell_south == cell_east):
+            south = [1, 1]
+            possibilities.append(south)
+        return possibilities
