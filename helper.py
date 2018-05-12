eb69464f903eaf1e9c15c3e8d3432ead7da52275
William Caetano <williamcaetano14@gmail.com>
2018-05-12 03:04:19 -0300

    Added commentary for invert_zero_one method

--- helper.py
+++ helper.py
@@ -12,5 +12,8 @@ def is_even(number):
 def is_odd(number):
     return number % 2 == 1
 
+# Define invert_zero_one: Convert the integer number 1 to 0 or 0 to 1.
+# Parameter => number: A integer value.
+# Return: 1 if number is zero, 0 otherwise.
 def invert_zero_one(number):
     return int(not bool(number))
