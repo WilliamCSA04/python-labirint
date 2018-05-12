42147e50a23aad02df1691b6ccc6fa941071b6ba
William Caetano <williamcaetano14@gmail.com>
2018-05-11 23:44:31 -0300

    Added default param to next_step_randomly_without_diagonals method

--- labirintHelper.py
+++ labirintHelper.py
@@ -1,10 +1,10 @@
 import random
 
-def next_step_randomly_without_diagonals(coordinates):
-    if coordinates is None:
+def next_step_randomly_without_diagonals(coordinate = None):
+    if coordinate is None:
         next_column = random.randint(-1, 1)
         next_row = random.randint(0, 1)
         if next_column is 1 or next_column is -1:
             next_row = 0
         return [next_row, next_column]
-    return random.choice(coordinates)
+    return random.choice(coordinate)
